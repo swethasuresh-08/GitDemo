e2fd46621945c401599f033d65f12ae6df580457
swetha <swethassuresh08@gmail.com>
2025-12-26 12:37:29 +0530

    Added maxSpeed in Car

--- src/Main.java
+++ src/Main.java
@@ -9,9 +9,9 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
     System.out.println("Car demo");
-    Car c1=new Car(10000,1200);
-    Car c2=new Car(20000,2500);
-    Car c3=new Car(35000,5600);
+    Car c1=new Car(10000,1200,50);
+    Car c2=new Car(20000,2500,1500);
+    Car c3=new Car(35000,5600,2550);
     List<Car> carList=new ArrayList<>(Arrays.asList(c1,c2,c3));
     carList.sort(new CarPriceComparator());
     System.out.println(carList);
